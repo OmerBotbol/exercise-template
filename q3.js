4569693fd2de6d8921261ec3d17b990d9a7eaba2
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-01-19 17:41:49 +0200

    adding q3

---
+++ q3.js
@@ -0,0 +1,20 @@
+function run() {
+    const inputArr = document.getElementById("input").value;
+    const inputN = document.getElementById("input2").value;
+    const output = document.getElementById("output");
+    let out = [];
+    let myArr = JSON.parse(inputArr);
+    let n = JSON.parse(inputN);
+    if(n === undefined){
+        out[0] = myArr[0];
+    }
+    else{
+        for(i = 0 ; i < n ; i++){
+            if (myArr[i] === undefined){
+                break;
+            }
+            out[i] = myArr[i];
+        }
+    }
+    output.innerText = out;
+}
