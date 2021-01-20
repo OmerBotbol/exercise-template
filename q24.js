569f33b235a9c6e7eb5581c351659c2543c4f712
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-01-20 18:08:00 +0200

    fixing 24

--- q24.js
+++ q24.js
@@ -1,12 +1,8 @@
 function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
-    const myArr = eval(input);
+    let myArr = eval(input);
     let out =[];
-    for (let i = 0; i < myArr.length; i++) {
-        if(!(isNaN(myArr[i] / 0))){
-            out.push(myArr[i])
-    }
-    }
+    out = myArr.filter(Boolean)
     output.innerText = out;
 }
