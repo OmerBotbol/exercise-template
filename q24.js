0ffeba4e90c4ed7ce84aed8c400bb92f65e6da8c
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-01-20 17:49:01 +0200

    adding q24

---
+++ q24.js
@@ -0,0 +1,12 @@
+function run() {
+    const input = document.getElementById("input").value;
+    const output = document.getElementById("output");
+    const myArr = eval(input);
+    let out =[];
+    for (let i = 0; i < myArr.length; i++) {
+        if(!(isNaN(myArr[i] / 0))){
+            newArr.push(myArr[i])
+    }
+    }
+    output.innerText = out;
+}
