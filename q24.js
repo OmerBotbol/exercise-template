d904d6f860371bb9fb905c6db2e127eb85f25d6c
OmerBotbol <77241676+OmerBotbol@users.noreply.github.com>
2021-01-21 00:36:54 +0200

    fixing q24

--- q24.js
+++ q24.js
@@ -3,6 +3,13 @@ function run() {
     const output = document.getElementById("output");
     let myArr = eval(input);
     let out =[];
-    out = myArr.filter(Boolean)
+    for (let i = 0; i < myArr.length; i++) {
+        if(typeof(myArr[i]) === "string" && myArr[i] !== ""){
+            out.push(myArr[i]);
+        }
+        if (myArr[i] / 0 === Infinity || myArr[i] / 0 === -Infinity) {
+            out.push(myArr[i]);
+        }
+    }
     output.innerText = out;
 }
